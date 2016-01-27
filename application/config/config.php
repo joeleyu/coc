4f2db877608bda73cc9ca7fe916626f29df41803
eyuel Joel <eyujazz@gmail.com>
2016-01-26 23:04:59 -0800

    initial code

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/e-learn/coc/';
+$config['base_url'] = 'http://localhost/coc/coc/';
 
 /*
 |--------------------------------------------------------------------------
